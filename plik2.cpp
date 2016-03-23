752a666640efda309dc7531c2a82872e2a2698e8
Mateusz Konieczny <mateuszkonieczny1992@gmail.com>
2016-03-23 15:11:19 +0100

    Commit piaty

--- plik2.cpp
+++ plik2.cpp
@@ -1,5 +1,5 @@
-prinfffffffftf (\\"Enter the last number (n)  of  the series\n\\");
+prisdasdasnfffffffftf (\\"Enter the last number (n)  of  the series\n\\");
 scanf (\\"%d\\", &n);
-if (n<=5) /* if n is a negative  number*/
+if (n<=22) /* if n is a negative  number*/
 printf (\\"Error. You gave a  negative number. Program ends.\n\\");
 return 0;
